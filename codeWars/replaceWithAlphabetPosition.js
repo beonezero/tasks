e181786edef3095fdde46399ef129c04c47105c5
Neakhai Yauheni <beonezero@protonmail.com>
2024-01-13 21:31:20 +0300

    refactor replaceWithAlphabetPosition.js

--- codeWars/replaceWithAlphabetPosition.js
+++ codeWars/replaceWithAlphabetPosition.js
@@ -7,7 +7,7 @@
 // "a" = 1, "b" = 2, etc.
 //
 //     Example
-// alphabetPosition("The sunset sets at twelve o' clock.")
+// alphabetPosition("The sunset sets at twelve o'clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 function alphabetPosition(text) {

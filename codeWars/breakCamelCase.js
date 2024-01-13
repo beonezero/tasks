ff4736e5f16f35ac460c01a3cf42ae6d1e2c1f1f
Neakhai Yauheni <beonezero@protonmail.com>
2024-01-13 20:23:46 +0300

    add task breakCamelCase.js

---
+++ codeWars/breakCamelCase.js
@@ -0,0 +1,19 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+//
+//     Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// complete the function
+function solution(string) {
+    let result = ""
+    for (let i = 0; i < string.length; i++){
+        if (string[i] === string[i].toUpperCase()){
+            result += ` ${string[i]}`
+        } else {
+            result += string[i]
+        }
+    }
+    return result
+}
